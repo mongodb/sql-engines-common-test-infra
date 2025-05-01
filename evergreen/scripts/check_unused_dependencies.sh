b54836d5a81dd0109bed5974202d89713df949f9
Matthew Chiaravalloti <matthew.chiaravalloti@mongodb.com>
2025-05-01 17:58:19 -0400

    SQL-2804: Convert unused dependencies function to script

---
+++ evergreen/scripts/check_unused_dependencies.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o errexit
+
+cargo install cargo-machete
+cargo build --all-features
+set +e
+cargo machete
+RETURN=$?
+set -e
+if [ $RETURN -ne 0 ]; then
+  >&2 echo "Unused dependencies found"
+  >&2 cargo machete
+  exit 1
+fi
