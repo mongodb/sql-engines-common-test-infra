71a2310cd5488b798b172324f04a7e8f76c8fe68
Oliver Bucaojit <oliver.bucaojit@mongodb.com>
2025-05-06 09:57:36 -0700

    SQL-2776: Create unified run_adf.sh script

---
+++ evergreen/scripts/update_orchestrator_mongod_port.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -o errexit
+
+if [[ -z "$DRIVERS_TOOLS" ]]; then
+    echo >&2 "\$DRIVERS_TOOLS must be set"
+    exit 1
+fi
+
+# Determine the target port:
+# Use MDB_TEST_LOCAL_PORT if it's set and not empty, otherwise use DEFAULT_PORT.
+DEFAULT_PORT=28017
+TARGET_PORT="${MDB_TEST_LOCAL_PORT:-$DEFAULT_PORT}"
+# use CONFIG_FILE if provided, otherwise use default basic.json
+CONFIG_FILE="$1"
+if [[ -z "$CONFIG_FILE" ]]; then
+  CONFIG_FILE="basic.json"
+fi
+CONFIG_PATH="${DRIVERS_TOOLS}/.evergreen/orchestration/configs/servers/${CONFIG_FILE}"
+
+
+cp "$CONFIG_PATH" "$CONFIG_PATH.bak"
+
+if ! sed -E -e "s/\"port\": [0-9]+/\"port\": $TARGET_PORT/" "$CONFIG_PATH" > "$CONFIG_PATH.tmp"; then
+  echo >&2 "sed failed to update port"
+  mv "$CONFIG_PATH.bak" "$CONFIG_PATH"
+  exit 1
+fi
+
+mv "$CONFIG_PATH.tmp" "$CONFIG_PATH"
+
+echo "Updated port to $TARGET_PORT in $CONFIG_PATH"
