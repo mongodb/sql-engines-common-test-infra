a1ced7cb87efba6496d38b931134063305031dbf
Oliver Bucaojit <oliver.bucaojit@mongodb.com>
2025-06-13 15:23:17 -0700

    SQL-2864: Use temp dir

--- evergreen/scripts/start_compute_node.sh
+++ evergreen/scripts/start_compute_node.sh
@@ -13,12 +13,12 @@ export COMPUTE_MODE_MONGOD_PORT=${COMPUTE_MODE_MONGOD_PORT:-47017}
 
 echo "Starting mongod compute node on port $COMPUTE_MODE_MONGOD_PORT..."
 
-mkdir -p "$DRIVERS_TOOLS/mongodb/compute_node"
+COMPUTE_NODE_DIR=/tmp/compute_node
+mkdir -p "$COMPUTE_NODE_DIR"
 "$DRIVERS_TOOLS/mongodb/bin/mongod" --port "$COMPUTE_MODE_MONGOD_PORT" \
                                   --setParameter enableComputeMode=1 \
-                                  --dbpath "$DRIVERS_TOOLS/mongodb/compute_node" \
-                                  --logpath "$DRIVERS_TOOLS/mongodb/compute_node/mongod.log" \
+                                  --dbpath "$COMPUTE_NODE_DIR" \
+                                  --logpath "$COMPUTE_NODE_DIR/mongod.log" \
                                   --fork
 
 echo "Successfully launched mongod compute node."
-
