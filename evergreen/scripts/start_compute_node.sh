206a77b58f7f640ca886e02accb1f45df4cef64f
Oliver Bucaojit <oliver.bucaojit@mongodb.com>
2025-06-13 15:11:57 -0700

    change dir

--- evergreen/scripts/start_compute_node.sh
+++ evergreen/scripts/start_compute_node.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
 set -o errexit
+# Starts a mongod compute node for ADF integration tests that require it.
+# ${DRIVERS_TOOLS}/.evergreen/run-orchestration.sh must be run before this script to set up
+# the mongod binary and directory.
 
 if [[ -z "$DRIVERS_TOOLS" ]]; then
     echo >&2 "Error: The \$DRIVERS_TOOLS environment variable must be set."
@@ -11,11 +13,13 @@ export COMPUTE_MODE_MONGOD_PORT=${COMPUTE_MODE_MONGOD_PORT:-47017}
 
 echo "Starting mongod compute node on port $COMPUTE_MODE_MONGOD_PORT..."
 
-mkdir -p "$DRIVERS_TOOLS/mongodb/compute_node"
+COMPUTE_NODE_DIR=/tmp/compute_node
+mkdir -p "$COMPUTE_NODE_DIR"
 "$DRIVERS_TOOLS/mongodb/bin/mongod" --port "$COMPUTE_MODE_MONGOD_PORT" \
                                   --setParameter enableComputeMode=1 \
-                                  --dbpath "$DRIVERS_TOOLS/mongodb/compute_node" \
-                                  --logpath "$DRIVERS_TOOLS/mongodb/compute_node/mongod.log" \
+                                  --dbpath "$COMPUTE_NODE_DIR" \
+                                  --logpath "$COMPUTE_NODE_DIR/mongod.log" \
                                   --fork
 
 echo "Successfully launched mongod compute node."
+
