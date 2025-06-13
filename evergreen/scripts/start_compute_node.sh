5a04c2a2b668dcc1058de1814478b4ee1f5039d4
Oliver Bucaojit <oliver.bucaojit@mongodb.com>
2025-06-13 15:07:37 -0700

    SQL-2864: Add script to start compute node

---
+++ evergreen/scripts/start_compute_node.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -o errexit
+# Starts a mongod compute node for ADF integration tests that require it.
+# ${DRIVERS_TOOLS}/.evergreen/run-orchestration.sh must be run before this script to set up
+# the mongod binary and directory.
+
+if [[ -z "$DRIVERS_TOOLS" ]]; then
+    echo >&2 "Error: The \$DRIVERS_TOOLS environment variable must be set."
+    exit 1
+fi
+
+export COMPUTE_MODE_MONGOD_PORT=${COMPUTE_MODE_MONGOD_PORT:-47017}
+
+echo "Starting mongod compute node on port $COMPUTE_MODE_MONGOD_PORT..."
+
+mkdir -p "$DRIVERS_TOOLS/mongodb/compute_node"
+"$DRIVERS_TOOLS/mongodb/bin/mongod" --port "$COMPUTE_MODE_MONGOD_PORT" \
+                                  --setParameter enableComputeMode=1 \
+                                  --dbpath "$DRIVERS_TOOLS/mongodb/compute_node" \
+                                  --logpath "$DRIVERS_TOOLS/mongodb/compute_node/mongod.log" \
+                                  --fork
+
+echo "Successfully launched mongod compute node."
+
