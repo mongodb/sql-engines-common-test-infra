4239c016f914bac4dbee5c4c41f5658854622451
Patrick Meredith <pmeredit@protonmail.com>
2025-07-22 11:57:01 -0400

    SQL-2909: Move users config to separate file

--- test-environment/run_adf.sh
+++ test-environment/run_adf.sh
@@ -71,6 +71,7 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 LOCAL_INSTALL_DIR=$SCRIPT_DIR/local_adf
 LOGS_PATH=$LOCAL_INSTALL_DIR/logs
 DB_CONFIG_PATH=$SCRIPT_DIR/configuration/adf_db_config.json
+USERS_CONFIG_PATH=$SCRIPT_DIR/configuration/adf_users_config.json
 # This config enables match filters after currentOp
 ADF_CONFIG_PATH=$SCRIPT_DIR/configuration/adf_config.yaml
 MONGOD_PORT=28017
@@ -253,37 +254,11 @@ if [ $ARG = $START ]; then
     STORES=$(printf "$STORES" "${MONGOD_PORT}")
     DATABASES=$(cat $DB_CONFIG_PATH)
     # add a user that only has read role for db2, it will have the same password 'pencil' as mhuser
-    DBREADERUSER=$(cat <<EOV
-{
-  "name": "db2reader",
-  "db": "admin",
-  "credentials": {
-    "scramSHA1": {
-      "iterations": 10000,
-      "salt": "CVmjg1IDp3KfHfiXNZhmbQ==",
-      "storedKey": "9cIonYrtLI/+OmZ96wiQkduwXeI=",
-      "serverKey": "kCfqkc7sgQaihzXHqbxZxz3O3H4="
-    },
-    "scramSHA256": {
-      "iterations": 15000,
-      "salt": "6i+wBCHE21EaI8IF9+tvPkCoYWY+IyedLaVXGQ==",
-      "storedKey": "hnn0iJZ6ZNO9X34oyxtZUKLAMScwpCdch3vGPuDXOvM=",
-      "serverKey": "3Qg6ptTiTvPwhGSZi0NpAkmsW8P5C/J46Is1t7xaIho="
-    }
-  },
-  "roles": [
-    {
-      "role": "read",
-      "db": "db2"
-    }
-  ],
-  "x509Type": "NONE",
-  "oidcAuthType": "NONE",
-  "awsIAMType": "NONE",
-  "createdDate": "0001-01-01T00:00:00Z"
-}
-EOV
-)
+    USERS=$(cat $USERS_CONFIG_PATH)
+
+    echo "!!!!!!!!"
+    echo "$USERS"
+    echo "!!!!!!!!"
 
     # Replace the existing storage config with a wildcard collection for the local mongodb
     cp ${TENANT_CONFIG} ${TENANT_CONFIG}.orig
@@ -292,7 +267,7 @@ EOV
                                                                && mv ${TENANT_CONFIG}.tmp ${TENANT_CONFIG}
     $JQ --argjson obj "$DATABASES" '.storage.databases += $obj' ${TENANT_CONFIG} > ${TENANT_CONFIG}.tmp\
                                                                && mv ${TENANT_CONFIG}.tmp ${TENANT_CONFIG}
-    $JQ --argjson obj "$DBREADERUSER" '.security.users += [$obj]' ${TENANT_CONFIG} > ${TENANT_CONFIG}.tmp\
+    $JQ --argjson obj "$USERS" '.security.users += $obj' ${TENANT_CONFIG} > ${TENANT_CONFIG}.tmp\
                                                                && mv ${TENANT_CONFIG}.tmp ${TENANT_CONFIG}
 
     $GO run cmd/buildscript/build.go init:mongodb-tenant
