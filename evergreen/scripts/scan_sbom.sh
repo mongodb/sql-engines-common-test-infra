1f90737cb147345a91e1a19c3ecffbb56168a669
Matthew Chiaravalloti <matthew.chiaravalloti@mongodb.com>
2026-01-26 16:30:48 -0500

    SQL-2997: Update sbom tasks to be generic

--- evergreen/scripts/scan_sbom.sh
+++ evergreen/scripts/scan_sbom.sh
@@ -4,17 +4,18 @@ set -o errexit
 
 echo ">>>> Scan SBOM for vulnerabilities..."
 if [[ "$ALLOW_VULNS" != "" ]]; then
+  echo "Vulnerability ids to ignore : $ALLOW_VULNS"
 
-echo "-- Generate .grype.yaml specifying vulnerabilities to ignore --"
-GRYPE_CONF_FILE=".grype.yaml"
-touch $GRYPE_CONF_FILE
-echo "ignore:" > $GRYPE_CONF_FILE
+  echo "-- Generate .grype.yaml specifying vulnerabilities to ignore --"
+  GRYPE_CONF_FILE=".grype.yaml"
+  touch $GRYPE_CONF_FILE
+  echo "ignore:" > $GRYPE_CONF_FILE
 
-IFS=','; for VULN_ID in $ALLOW_VULNS; do
-    echo "Ignoring vulnerability with id $VULN_ID"
-    echo "    - vulnerability: $VULN_ID" >> $GRYPE_CONF_FILE
-done
-echo "------------------------------------"
+  IFS=','; for VULN_ID in $ALLOW_VULNS; do
+      echo "Ignoring vulnerability with id $VULN_ID"
+      echo "    - vulnerability: $VULN_ID" >> $GRYPE_CONF_FILE
+  done
+  echo "------------------------------------"
 fi
 
 echo "-- Scanning dependency for vulnerabilities --"
