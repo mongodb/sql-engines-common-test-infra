d24b78ef1babdd499a84b7a2828c89cc95dc7bb6
Evan Fetsko <evan.fetsko@mongodb.com>
2026-07-24 12:00:49 -0400

    use DevProd ECR for container images instead of Artifactory

--- evergreen/scripts/augment_sbom.sh
+++ evergreen/scripts/augment_sbom.sh
@@ -8,6 +8,6 @@ echo "SBOM_OUT_PATH = $sbom_out_path"
 echo "-- Augmenting SBOM Lite --"
 docker run -i --platform="linux/amd64" --rm -v "$PWD":/pwd \
   --env-file ./aws_vars.env \
-  artifactory.corp.mongodb.com/release-tools-container-registry-public-local/silkbomb:2.0 \
+  901841024863.dkr.ecr.us-east-1.amazonaws.com/release-infrastructure/silkbomb:2.0 \
   augment --repo $github_org/$github_repo --branch $branch_name --sbom-in /pwd/$sbom_in_path --sbom-out /pwd/$sbom_out_path --force
 echo "-------------------------------"
